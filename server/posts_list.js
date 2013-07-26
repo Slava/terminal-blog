13505a95d3f0f9ff1e5691175efd529f54a310e3
Slava Kim <slava@meteor.com>
2013-07-26 15:45:54 -0700

    JS hoisting dummy post

--- server/posts_list.js
+++ server/posts_list.js
@@ -3,19 +3,19 @@ var postsList = [
     id: 'srm-571',
     title:'SRM 571',
     date: new Date(2013,1,19),
-    tags: []
+    tags: ['TC']
   },
   {
     id: 'tco-2013-2a',
     title:'TCO 2013 2A',
     date: new Date(2013,2,31),
-    tags: []
+    tags: ['TC']
   },
   {
     id: 'tco-2013-qual-1a',
     title:'TCO 2013 Qual 1A',
     date: new Date(2013,1,23),
-    tags: []
+    tags: ['TC']
   },
   {
     id: 'when-single-paged-apps-suck',
@@ -39,7 +39,7 @@ var postsList = [
     id: 'fibers',
     title:'Fibers',
     date: new Date(2013,4,24),
-    tags: []
+    tags: ['js', 'fibers']
   },
   {
     id: 'applescript',
@@ -51,20 +51,26 @@ var postsList = [
     id: 'less-known-y-combinator',
     title:'Less known Y combinator',
     date: new Date(2013,2,30),
-    tags: []
+    tags: ['yc', 'js', 'fun']
   },
   {
     id: 'automata-revelation',
     title: 'Pushdown Automata Revelation',
     date: new Date(2013,5,7),
-    tags: []
+    tags: ['CS', 'automata']
   },
   {
     id: 'dont-go-back-to-school',
     title:'Dont go back to school',
     date: new Date(2013,5,25),
-    tags: []
-  } 
+    tags: ['book', 'school']
+  },
+  {
+    id: 'js-hoisting',
+    title: 'JS hoisting',
+    date: new Date(2013,6,26),
+    tags: ['javascript']
+  }
 ];
 
 posts = new Meteor.Collection('posts');
